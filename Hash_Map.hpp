df6e690b9fff0af47f55173a5d52b53300d65d2e
thomasqvan <78669043+thomasqvan@users.noreply.github.com>
2021-03-21 16:03:33 -0700

    Update and rename HashSet.hpp to Hash_Map.hpp

--- Hash_Map.hpp
+++ Hash_Map.hpp
@@ -1,106 +1,33 @@
-// HashSet.hpp
-//
-// ICS 46 Winter 2021
-// Project #4: Set the Controls for the Heart of the Sun
-//
-// A HashSet is an implementation of a Set that is a separately-chained
-// hash table, implemented as a dynamically-allocated array of linked
-// lists.  At any given time, the HashSet has a "size" indicating
-// how many elements are stored within it, along with a "capacity"
-// indicating the size of the array.
-//
-// As elements are added to the HashSet and the proportion of the HashSet's
-// size to its capacity exceeds 0.8 (i.e., there are more than 80% as many
-// elements as there are array cells), the HashSet should be resized so
-// that it is twice as large as it was before.
-//
-// You are not permitted to use the containers in the C++ Standard Library
-// (such as std::set, std::map, or std::vector) to store the information
-// in your data structure.  Instead, you'll need to use a dynamically-
-// allocated array and your own linked list implemenation; the linked list
-// doesn't have to be its own class, though you can do that, if you'd like.
-
-#ifndef HASHSET_HPP
-#define HASHSET_HPP
+// Hash_Map.hpp
+#ifndef HASH_MAP_HPP
+#define HASH_MAP_HPP
 
 #include <functional>
 #include "Set.hpp"
-
 #include "StringHashing.hpp"
-
 
 template <typename ElementType>
 class HashSet : public Set<ElementType>
 {
 public:
-    // The default capacity of the HashSet before anything has been
-    // added to it.
     static constexpr unsigned int DEFAULT_CAPACITY = 10;
-
-    // A HashFunction is a function that takes a reference to a const
-    // ElementType and returns an unsigned int.
     using HashFunction = std::function<unsigned int(const ElementType&)>;
 
 public:
-    // Initializes a HashSet to be empty, so that it will use the given
-    // hash function whenever it needs to hash an element.
     explicit HashSet(HashFunction hashFunction);
-
-    // Cleans up the HashSet so that it leaks no memory.
     ~HashSet() noexcept override;
 
-    // Initializes a new HashSet to be a copy of an existing one.
     HashSet(const HashSet& s);
-
-    // Initializes a new HashSet whose contents are moved from an
-    // expiring one.
     HashSet(HashSet&& s) noexcept;
 
-    // Assigns an existing HashSet into another.
     HashSet& operator=(const HashSet& s);
-
-    // Assigns an expiring HashSet into another.
     HashSet& operator=(HashSet&& s) noexcept;
 
-
-    // isImplemented() should be modified to return true if you've
-    // decided to implement a HashSet, false otherwise.
     bool isImplemented() const noexcept override;
-
-
-    // add() adds an element to the set.  If the element is already in the set,
-    // this function has no effect.  This function triggers a resizing of the
-    // array when the ratio of size to capacity would exceed 0.8, in which case
-    // the new capacity should be determined by this formula:
-    //
-    //     capacity * 2 + 1
-    //
-    // In the case where the array is resized, this function runs in linear
-    // time (with respect to the number of elements, assuming a good hash
-    // function); otherwise, it runs in constant time (again, assuming a good
-    // hash function).  The amortized running time is also constant.
     void add(const ElementType& element) override;
-
-
-    // contains() returns true if the given element is already in the set,
-    // false otherwise.  This function runs in constant time (with respect
-    // to the number of elements, assuming a good hash function).
     bool contains(const ElementType& element) const override;
-
-
-    // size() returns the number of elements in the set.
     unsigned int size() const noexcept override;
-
-
-    // elementsAtIndex() returns the number of elements that hashed to a
-    // particular index in the array.  If the index is out of the boundaries
-    // of the array, this function returns 0.
     unsigned int elementsAtIndex(unsigned int index) const;
-
-
-    // isElementAtIndex() returns true if the given element hashed to a
-    // particular index in the array, false otherwise.  If the index is
-    // out of the boundaries of the array, this functions returns 0.
     bool isElementAtIndex(const ElementType& element, unsigned int index) const;
 
 private:
@@ -443,4 +370,4 @@ bool HashSet<ElementType>::isElementAtIndex(const ElementType& element,
     return false;
 }
 
-#endif // HASHSET_HPP
+#endif // HASH_MAP_HPP
