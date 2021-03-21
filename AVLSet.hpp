b0f0aeb136b3c183c06fb988feb795d2463afddd
thomasqvan <78669043+thomasqvan@users.noreply.github.com>
2021-03-21 15:51:22 -0700

    Update AVLSet.hpp

--- AVLSet.hpp
+++ AVLSet.hpp
@@ -1,106 +1,44 @@
 // AVLSet.hpp
-//
-// ICS 46 Winter 2021
-// Project #4: Set the Controls for the Heart of the Sun
-//
-// An AVLSet is an implementation of a Set that is an AVL tree, which uses
-// the algorithms we discussed in lecture to maintain balance every time a
-// new element is added to the set.  The balancing is actually optional,
-// with a bool parameter able to be passed to the constructor to explicitly
-// turn the balancing on or off (on is default).  If the balancing is off,
-// the AVL tree acts like a binary search tree (e.g., it will become
-// degenerate if elements are added in ascending order).
-//
-// You are not permitted to use the containers in the C++ Standard Library
-// (such as std::set, std::map, or std::vector) to store the information
-// in your data structure.  Instead, you'll need to implement your AVL tree
-// using your own dynamically-allocated nodes, with pointers connecting them,
-// and with your own balancing algorithms used.
-
 #ifndef AVLSET_HPP
 #define AVLSET_HPP
 
 #include <functional>
-#include "Set.hpp"
-
 #include<algorithm>
 
 template <typename ElementType>
 class AVLSet : public Set<ElementType>
 {
 public:
-    // A VisitFunction is a function that takes a reference to a const
-    // ElementType and returns no value.
     using VisitFunction = std::function<void(const ElementType&)>;
 
 public:
-    // Initializes an AVLSet to be empty, with or without balancing.
     explicit AVLSet(bool shouldBalance = true);
 
-    // Cleans up the AVLSet so that it leaks no memory.
     ~AVLSet() noexcept override;
 
-    // Initializes a new AVLSet to be a copy of an existing one.
     AVLSet(const AVLSet& s);
 
-    // Initializes a new AVLSet whose contents are moved from an
-    // expiring one.
     AVLSet(AVLSet&& s) noexcept;
 
-    // Assigns an existing AVLSet into another.
     AVLSet& operator=(const AVLSet& s);
 
-    // Assigns an expiring AVLSet into another.
     AVLSet& operator=(AVLSet&& s) noexcept;
 
-
-    // isImplemented() should be modified to return true if you've
-    // decided to implement an AVLSet, false otherwise.
     bool isImplemented() const noexcept override;
 
-
-    // add() adds an element to the set.  If the element is already in the set,
-    // this function has no effect.  This function always runs in O(log n) time
-    // when there are n elements in the AVL tree.
     void add(const ElementType& element) override;
-
-
-    // contains() returns true if the given element is already in the set,
-    // false otherwise.  This function always runs in O(log n) time when
-    // there are n elements in the AVL tree.
+    
     bool contains(const ElementType& element) const override;
 
-
-    // size() returns the number of elements in the set.
     unsigned int size() const noexcept override;
 
-
-    // height() returns the height of the AVL tree.  Note that, by definition,
-    // the height of an empty tree is -1.
     int height() const noexcept;
-
-
-    // preorder() calls the given "visit" function for each of the elements
-    // in the set, in the order determined by a preorder traversal of the AVL
-    // tree.
+    
     void preorder(VisitFunction visit) const;
-
-
-    // inorder() calls the given "visit" function for each of the elements
-    // in the set, in the order determined by an inorder traversal of the AVL
-    // tree.
     void inorder(VisitFunction visit) const;
-
-
-    // postorder() calls the given "visit" function for each of the elements
-    // in the set, in the order determined by a postorder traversal of the AVL
-    // tree.
     void postorder(VisitFunction visit) const;
 
-
 private:
-    // You'll no doubt want to add member variables and "helper" member
-    // functions here.
     
     struct Node {
     
